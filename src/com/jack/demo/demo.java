2ed1b50b6612d73d875091c10515ed0fb993cf4a
jack <13925818278@163.com>
2018-01-22 22:47:00 +0800

    first commit

---
+++ src/com/jack/demo/demo.java
@@ -0,0 +1,12 @@
+package com.jack.demo;
+
+public class demo {
+
+
+    public static void main(String[] args) {
+        String a = "我玩完不给钱,就不算卖了!";
+        System.out.println("a = [" + a + "]");
+        System.out.println("你好吗");
+    }
+
+}
